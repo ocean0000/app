f09662680bdf91a0a4f7154d7f23e6431bee77e7
ocean0000 <duongbui2612@gmail.com>
2024-11-13 07:21:11 +0700

    add a bot

--- src/main/game_1_src/game_1.py
+++ src/main/game_1_src/game_1.py
@@ -6,16 +6,21 @@ class Game_1:
       self.background = pygame.image.load(game_1_background_image)
       self.background = pygame.transform.scale(self.background, (window_width, window_height))
       self.displaySurface  = displaySurface
-      self.bot1= Bot((500,500))
+      self.bot1= Bot((500,500),1)
+      self.bot2 = Bot((100,100),2)
+      self.bot1.target_bot = self.bot2
+      self.bot2.target_bot = self.bot1
       self.clock = pygame.time.Clock()
 
    def update(self):
       self.bot1.update()
+      self.bot2.update()
    
 
    def draw(self):
       self.displaySurface.blit(self.background, (0, 0))
       self.bot1.draw(self.displaySurface)
+      self.bot2.draw(self.displaySurface)
 
 
    def run(self):
